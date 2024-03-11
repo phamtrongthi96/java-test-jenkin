18e8176b90b095ac8da2908f0fdabf069f6649e1
thi pham <sniperbingx@gmail.com>
2024-03-11 17:20:05 +0700

    first commit

---
+++ Demo/src/main/java/org/example/Main.java
@@ -0,0 +1,71 @@
+package org.example;
+
+import java.util.List;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        List<String> list  = List.of("mit", "hai", "ba");
+
+
+        Thread t1 = new Thread(new MyRunnable(), "t1");
+        Thread t2 = new Thread(new MyRunnable(), "t2");
+        Thread t3 = new Thread(new MyRunnable(), "t3");
+
+        t1.start();
+
+        //start second thread after waiting for 2 seconds or if it's dead
+        try {
+            t1.join(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        t2.start();
+
+        //start third thread only when first thread is dead
+        try {
+            t1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        t3.start();
+
+//        let all threads finish execution before finishing main thread
+        try {
+            t1.join();
+            t2.join();
+            t3.join();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        System.out.println("All threads are dead, exiting main thread");
+    }
+
+
+
+
+}
+
+class MyRunnable implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println("Thread started:::"+Thread.currentThread().getName());
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Thread ended:::"+Thread.currentThread().getName());
+    }
+
+}
