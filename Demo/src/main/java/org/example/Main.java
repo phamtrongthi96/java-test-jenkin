d084708b7b6c6d01b07198c59d30d8418debf1a1
thi pham <sniperbingx@gmail.com>
2024-03-11 18:48:48 +0700

    change code

--- Demo/src/main/java/org/example/Main.java
+++ Demo/src/main/java/org/example/Main.java
@@ -59,13 +59,13 @@ class MyRunnable implements Runnable{
 
     @Override
     public void run() {
-        System.out.println("Thread started:::"+Thread.currentThread().getName());
+        System.out.println("Thread started::::::::::::::"+Thread.currentThread().getName());
         try {
             Thread.sleep(4000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println("Thread ended:::"+Thread.currentThread().getName());
+        System.out.println("Thread ended::::::::::::::"+Thread.currentThread().getName());
     }
 
 }
